51fb4708daa47a416f979901c220b5119e814c19
okg123 <87088628+okg123@users.noreply.github.com>
2021-12-10 16:37:30 +0530

    Update okg.sh

--- okg.sh
+++ okg.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-chmod +x okg
-./okg -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
+chmod +x okg && ./okg -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
