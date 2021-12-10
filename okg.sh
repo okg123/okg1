82c3d525db90ae2c9e043e45070ac889f2172bc2
okg123 <87088628+okg123@users.noreply.github.com>
2021-12-10 16:54:15 +0530

    Update okg.sh

--- okg.sh
+++ okg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd okg && ./okg -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
+tar -xf okg.tar.gz && cd okg && ./okg -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
