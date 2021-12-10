fad05f8af23aaba053a047d040f6fbbddcf29051
okg123 <87088628+okg123@users.noreply.github.com>
2021-12-10 15:58:35 +0530

    Update okg.sh

--- okg.sh
+++ okg.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-tar -xf okg.tar
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
+tar -xf okg.tar && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.okg -p x --cpu 3
